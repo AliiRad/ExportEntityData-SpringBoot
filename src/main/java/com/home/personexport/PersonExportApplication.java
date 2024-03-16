2137a435466c31188b025389a058a16fc25b873f
AliRad <alixrad77@gmail.com>
2024-03-16 20:59:11 +0330

    Add Pom and ApplicationStarter and Tester Class -AliiRad

---
+++ src/main/java/com/home/personexport/PersonExportApplication.java
@@ -0,0 +1,13 @@
+package com.home.personexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/personexport/PersonExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.personexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
