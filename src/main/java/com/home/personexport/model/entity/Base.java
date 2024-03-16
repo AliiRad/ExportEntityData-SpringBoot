ca61ce51de836786c8e29440872fb78e6c932b93
AliRad <alixrad77@gmail.com>
2024-03-16 20:59:28 +0330

    Add Base Class -AliiRad

---
+++ src/main/java/com/home/personexport/model/entity/Base.java
@@ -0,0 +1,15 @@
+package com.home.personexport.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Base {
+    private boolean active;
+    private boolean deleted;
+}
